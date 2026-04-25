6068347e117ce16e1387abc840a830663e97780b
Seko <me@sekoree.dev>
2026-04-26 00:19:59 +0200

    Forgotten

---
+++ UI/SPlayer/SPlayer.Core/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SPlayer.Core.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ UI/SPlayer/SPlayer.Core/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SPlayer.Core.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
